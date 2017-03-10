b640172ee51ff3fbb1a85f32edad187412aa9d02
Christopher Stock <email@christopherstock.de>
2017-03-10 11:32:42 +0100

    Updated classes in packages 'ui' and 'util'.

--- src/de/mayflower/mfg/ui/MfgCamera.ts
+++ src/de/mayflower/mfg/ui/MfgCamera.ts
@@ -1,10 +1,10 @@
 
-    /*****************************************************************************
+    /*******************************************************************************************************************
     *   Manages the camera that calculates the visible area for the user.
     *
     *   @author  Christopher Stock
     *   @version 1.0
-    *****************************************************************************/
+    *******************************************************************************************************************/
     class MfgCamera
     {
         /** Current horizontal offset. */
@@ -13,16 +13,16 @@
         /** Current vertical offset. */
         public              y                       :number                 = 0;
 
-        /*****************************************************************************
-        *   Constructs a new camera.
-        *****************************************************************************/
+        /***************************************************************************************************************
+        *   Creates a new camera.
+        ***************************************************************************************************************/
         public constructor()
         {
             this.x = 0;
             this.y = 0;
         }
 
-        /*****************************************************************************
+        /***************************************************************************************************************
         *   Updates the camera by reassigning the horizontal and vertical offset.
         *
         *   @param levelWidth   The width  of the surrounding level.
@@ -30,14 +30,14 @@
         *   @param canvasWidth  The width  of the 2D rendering context.
         *   @param canvasHeight The height of the 2D rendering context.
         *   @param subject      The rectangle to center the camera onto.
-        *****************************************************************************/
+        ***************************************************************************************************************/
         public update
         (
-            levelWidth:number,
-            levelHeight:number,
-            canvasWidth:number,
-            canvasHeight:number,
-            subject:MfgRect2D
+            levelWidth   :number,
+            levelHeight  :number,
+            canvasWidth  :number,
+            canvasHeight :number,
+            subject      :MfgRect2D
         )
         {
             //calculate scroll-x-offset so camera is centered to player
--- src/de/mayflower/mfg/ui/MfgDrawing.ts
+++ src/de/mayflower/mfg/ui/MfgDrawing.ts
@@ -1,10 +1,10 @@
 
-    /*****************************************************************************
+    /*******************************************************************************************************************
     *   Offers independent drawing functionality for the canvas.
     *
     *   @author  Christopher Stock
     *   @version 1.0
-    *****************************************************************************/
+    *******************************************************************************************************************/
     class MfgDrawing
     {
         /** The RGBA statement representing color 'white' with full opacity. */
@@ -19,10 +19,10 @@
         public      static      COLOR_RED_DARK_OPAQUE                   :string         = "rgba( 150, 0,   0,   1.0  )";
         /** The RGBA statement representing color 'yellow' with half opacity. */
         public      static      COLOR_YELLOW_TRANSLUCENT_50             :string         = "rgba( 255, 255, 0,   0.5  )";
-
+        /** The RGBA statement representing color 'black' with full opacity. */
         public      static      COLOR_BLACK_OPAQUE                      :string         = "rgba( 0,   0,   0,   1.0  )";
 
-        /*****************************************************************************
+        /***************************************************************************************************************
         *   Fills a rect with the specified dimensions and color.
         *
         *   @param ctx    The rendering context.
@@ -31,14 +31,14 @@
         *   @param width  The desired width.
         *   @param height The desired height.
         *   @param col    A fill color.
-        *****************************************************************************/
+        ***************************************************************************************************************/
         public static fillRect( ctx:CanvasRenderingContext2D, x:number, y:number, width:number, height:number, col:string )
         {
             ctx.fillStyle = col;
             ctx.fillRect( x, y, width, height );
         }
 
-        /*****************************************************************************
+        /***************************************************************************************************************
         *   Fills a rect with the specified dimensions and color.
         *
         *   @param ctx    The rendering context.
@@ -47,21 +47,21 @@
         *   @param width  The desired width.
         *   @param height The desired height.
         *   @param col    A fill color.
-        *****************************************************************************/
+        ***************************************************************************************************************/
         public static drawRect( ctx:CanvasRenderingContext2D, x:number, y:number, width:number, height:number, col:string )
         {
             ctx.strokeStyle = col;
             ctx.strokeRect( x, y, width, height );
         }
 
-        /*****************************************************************************
+        /***************************************************************************************************************
         *   Draws an image at the specified location with a specified anchor.
         *
         *   @param ctx The rendering context
         *   @param img The image to draw.
         *   @param x   Drawing position x.
         *   @param y   Drawing position y.
-        *****************************************************************************/
+        ***************************************************************************************************************/
         public static drawImage( ctx:CanvasRenderingContext2D, img:HTMLImageElement, x:number, y:number )
         {
             ctx.drawImage( img, x, y );
