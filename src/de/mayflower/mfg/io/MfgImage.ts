944aeee67cab02a3b1c47e4d7b79054e0b485fa4
Christopher Stock <email@christopherstock.de>
2017-03-10 11:18:40 +0100

    Updated classes in package 'io'.

--- src/de/mayflower/mfg/io/MfgImage.ts
+++ src/de/mayflower/mfg/io/MfgImage.ts
@@ -1,85 +1,85 @@
 
     /// <reference path="../MfgSetting.ts"/>
 
-    /*****************************************************************************
+    /*******************************************************************************************************************
     *   Specifies all different images being used in the game.
     *
     *   @author  Christopher Stock
     *   @version 1.0
-    *****************************************************************************/
+    *******************************************************************************************************************/
     class MfgImage
     {
         /** The image for the 'player default'. */
-        public      static      PLAYER_DEFAULT          :string                 = MfgSetting.PATH_IMAGE + "player/default.png";
+        public      static      PLAYER_DEFAULT          :string                 = "player/default.png";
         /** The image for the 'player crashed'. */
-        public      static      PLAYER_CRASHED          :string                 = MfgSetting.PATH_IMAGE + "player/crashed.png";
+        public      static      PLAYER_CRASHED          :string                 = "player/crashed.png";
 
         /** The image for the background. */
-        public      static      BG_1                    :string                 = MfgSetting.PATH_IMAGE + "bg/bg1.jpg";
+        public      static      BG_1                    :string                 = "bg/bg1.jpg";
 
         /** The image for the deco 'road straight'. */
-        public      static      DECO_ROAD_STRAIGHT      :string                 = MfgSetting.PATH_IMAGE + "deco/roadStraight.png";
+        public      static      DECO_ROAD_STRAIGHT      :string                 = "deco/roadStraight.png";
         /** The image for the deco 'road left'. */
-        public      static      DECO_ROAD_LEFT          :string                 = MfgSetting.PATH_IMAGE + "deco/roadLeft.png";
+        public      static      DECO_ROAD_LEFT          :string                 = "deco/roadLeft.png";
         /** The image for the deco 'road right'. */
-        public      static      DECO_ROAD_RIGHT         :string                 = MfgSetting.PATH_IMAGE + "deco/roadRight.png";
+        public      static      DECO_ROAD_RIGHT         :string                 = "deco/roadRight.png";
         /** The image for the deco 'finish line'. */
-        public      static      DECO_ROAD_FINISH_LINE   :string                 = MfgSetting.PATH_IMAGE + "deco/finishLine.png";
+        public      static      DECO_ROAD_FINISH_LINE   :string                 = "deco/finishLine.png";
         /** The image for the deco 'bridge'. */
-        public      static      DECO_BRIDGE             :string                 = MfgSetting.PATH_IMAGE + "deco/bridge.png";
+        public      static      DECO_BRIDGE             :string                 = "deco/bridge.png";
         /** The image for the deco 'field 1'. */
-        public      static      DECO_FIELD_1            :string                 = MfgSetting.PATH_IMAGE + "deco/field1.png";
+        public      static      DECO_FIELD_1            :string                 = "deco/field1.png";
         /** The image for the deco 'country road'. */
-        public      static      DECO_COUNTY_ROAD        :string                 = MfgSetting.PATH_IMAGE + "deco/countryRoad.png";
+        public      static      DECO_COUNTY_ROAD        :string                 = "deco/countryRoad.png";
 
         /** The image for the obstacle 'water 1'. */
-        public      static      OBSTACLE_WATER_1        :string                 = MfgSetting.PATH_IMAGE + "obstacle/water1.png";
+        public      static      OBSTACLE_WATER_1        :string                 = "obstacle/water1.png";
         /** The image for the obstacle 'water 2'. */
-        public      static      OBSTACLE_WATER_2        :string                 = MfgSetting.PATH_IMAGE + "obstacle/water2.png";
+        public      static      OBSTACLE_WATER_2        :string                 = "obstacle/water2.png";
 
         /** The image for the obstacle 'tree 1'. */
-        public      static      OBSTACLE_TREE_1         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree1.png";
+        public      static      OBSTACLE_TREE_1         :string                 = "obstacle/tree1.png";
         /** The image for the obstacle 'tree 2'. */
-        public      static      OBSTACLE_TREE_2         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree2.png";
+        public      static      OBSTACLE_TREE_2         :string                 = "obstacle/tree2.png";
         /** The image for the obstacle 'tree 3'. */
-        public      static      OBSTACLE_TREE_3         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree3.png";
+        public      static      OBSTACLE_TREE_3         :string                 = "obstacle/tree3.png";
         /** The image for the obstacle 'tree 4'. */
-        public      static      OBSTACLE_TREE_4         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree4.png";
+        public      static      OBSTACLE_TREE_4         :string                 = "obstacle/tree4.png";
         /** The image for the obstacle 'tree 5'. */
-        public      static      OBSTACLE_TREE_5         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree5.png";
+        public      static      OBSTACLE_TREE_5         :string                 = "obstacle/tree5.png";
         /** The image for the obstacle 'tree 6'. */
-        public      static      OBSTACLE_TREE_6         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree6.png";
+        public      static      OBSTACLE_TREE_6         :string                 = "obstacle/tree6.png";
         /** The image for the obstacle 'tree 7'. */
-        public      static      OBSTACLE_TREE_7         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree7.png";
+        public      static      OBSTACLE_TREE_7         :string                 = "obstacle/tree7.png";
         /** The image for the obstacle 'tree 8'. */
-        public      static      OBSTACLE_TREE_8         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree8.png";
+        public      static      OBSTACLE_TREE_8         :string                 = "obstacle/tree8.png";
         /** The image for the obstacle 'tree 9'. */
-        public      static      OBSTACLE_TREE_9         :string                 = MfgSetting.PATH_IMAGE + "obstacle/tree9.png";
+        public      static      OBSTACLE_TREE_9         :string                 = "obstacle/tree9.png";
 
         /** The image for the obstacle 'car 1'. */
-        public      static      OBSTACLE_CAR_1          :string                 = MfgSetting.PATH_IMAGE + "obstacle/car1.png";
+        public      static      OBSTACLE_CAR_1          :string                 = "obstacle/car1.png";
         /** The image for the obstacle 'car 2'. */
-        public      static      OBSTACLE_CAR_2          :string                 = MfgSetting.PATH_IMAGE + "obstacle/car2.png";
+        public      static      OBSTACLE_CAR_2          :string                 = "obstacle/car2.png";
         /** The image for the obstacle 'car 3'. */
-        public      static      OBSTACLE_CAR_3          :string                 = MfgSetting.PATH_IMAGE + "obstacle/car3.png";
+        public      static      OBSTACLE_CAR_3          :string                 = "obstacle/car3.png";
         /** The image for the obstacle 'car 4'. */
-        public      static      OBSTACLE_CAR_4          :string                 = MfgSetting.PATH_IMAGE + "obstacle/car4.png";
+        public      static      OBSTACLE_CAR_4          :string                 = "obstacle/car4.png";
         /** The image for the obstacle 'truck 1'. */
-        public      static      OBSTACLE_TRUCK_1        :string                 = MfgSetting.PATH_IMAGE + "obstacle/truck1.png";
+        public      static      OBSTACLE_TRUCK_1        :string                 = "obstacle/truck1.png";
         /** The image for the obstacle 'truck 2'. */
-        public      static      OBSTACLE_TRUCK_2        :string                 = MfgSetting.PATH_IMAGE + "obstacle/truck2.png";
+        public      static      OBSTACLE_TRUCK_2        :string                 = "obstacle/truck2.png";
         /** The image for the obstacle 'truck 3'. */
-        public      static      OBSTACLE_TRUCK_3        :string                 = MfgSetting.PATH_IMAGE + "obstacle/truck3.png";
+        public      static      OBSTACLE_TRUCK_3        :string                 = "obstacle/truck3.png";
         /** The image for the obstacle 'truck 4'. */
-        public      static      OBSTACLE_TRUCK_4        :string                 = MfgSetting.PATH_IMAGE + "obstacle/truck4.png";
+        public      static      OBSTACLE_TRUCK_4        :string                 = "obstacle/truck4.png";
         /** The image for the obstacle 'truck 5'. */
-        public      static      OBSTACLE_TRUCK_5        :string                 = MfgSetting.PATH_IMAGE + "obstacle/truck5.png";
+        public      static      OBSTACLE_TRUCK_5        :string                 = "obstacle/truck5.png";
 
         /** The image for the obstacle 'house 1'. */
-        public      static      OBSTACLE_HOUSE_1        :string                 = MfgSetting.PATH_IMAGE + "obstacle/house1.png";
+        public      static      OBSTACLE_HOUSE_1        :string                 = "obstacle/house1.png";
 
         /** The image for the obstacle 'sign 1'. */
-        public      static      OBSTACLE_SIGN_1         :string                 = MfgSetting.PATH_IMAGE + "obstacle/sign1.png";
+        public      static      OBSTACLE_SIGN_1         :string                 = "obstacle/sign1.png";
 
         /** This array contains all filenames of all images that shall be loaded. */
         public      static      FILE_NAMES              :Array<string>          =
--- src/de/mayflower/mfg/io/MfgSound.ts
+++ src/de/mayflower/mfg/io/MfgSound.ts
@@ -1,14 +1,14 @@
 
-    /*****************************************************************************
+    /*******************************************************************************************************************
     *   Specifies all different sound effects being used in the game.
     *
     *   @author  Christopher Stock
     *   @version 1.0
-    *****************************************************************************/
+    *******************************************************************************************************************/
     class MfgSound
     {
         /** The sound for the 'background - stickerbrush symphony'. */
-        public      static      SOUND_BG_TD2                    :string                 = MfgSetting.PATH_SOUND + "bg1.mp3";
+        public      static      SOUND_BG_TD2                    :string                 = "bg1.mp3";
 
         /** This array contains all filenames of all sounds that shall be loaded. */
         public      static      FILE_NAMES                      :Array<string>          =
